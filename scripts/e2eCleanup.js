3c33bdd593ff5c501596f7d6d91c750c153632e9
Oliver Saternus <mail@oliversaternus.de>
2022-04-05 22:31:52 +0200

    improve test cleanup

--- scripts/e2eCleanup.js
+++ scripts/e2eCleanup.js
@@ -1,18 +1,31 @@
-const { unlinkSync, renameSync, existsSync } = require('fs');
+const { unlinkSync, renameSync, existsSync, rmSync } = require('fs');
 const { join } = require('path');
 
 const testDbPath = join(__dirname, '../', 'prisma', 'test.db');
 const testDbJournalPath = join(__dirname, '../', 'prisma', 'test.db-journal');
 const prismaEnvPath = join(__dirname, '../', 'prisma', '.env');
 const tempPrismaEnvPath = join(__dirname, '../', 'prisma', 'temp.env');
+const migrationsFolder = join(__dirname, '../', 'prisma', 'migrations');
+const tempMigrationsFolder = join(__dirname, '../', 'prisma', 'temp-migrations');
 
+// delete test relevant files
 if (existsSync(testDbPath)) {
     unlinkSync(testDbPath);
 }
 if (existsSync(testDbJournalPath)) {
     unlinkSync(testDbJournalPath);
 }
+if (existsSync(migrationsFolder)) {
+    rmSync(migrationsFolder, { recursive: true, force: true });
+}
+if (existsSync(prismaEnvPath)) {
+    unlinkSync(prismaEnvPath);
+}
+
+// restore original files
 if (existsSync(tempPrismaEnvPath)) {
     renameSync(tempPrismaEnvPath, prismaEnvPath);
 }
-unlinkSync(prismaEnvPath);
+if (existsSync(tempMigrationsFolder)) {
+    renameSync(tempMigrationsFolder, migrationsFolder);
+}
--- scripts/e2ePrepare.js
+++ scripts/e2ePrepare.js
@@ -5,14 +5,23 @@ const prismaEnvPath = join(__dirname, '../', 'prisma', '.env');
 const tempPrismaEnvPath = join(__dirname, '../', 'prisma', 'temp.env');
 const testDbPath = join(__dirname, '../', 'prisma', 'test.db');
 const testDbJournalPath = join(__dirname, '../', 'prisma', 'test.db-journal');
+const migrationsFolder = join(__dirname, '../', 'prisma', 'migrations');
+const tempMigrationsFolder = join(__dirname, '../', 'prisma', 'temp-migrations');
 
+// eventually delete existing test files
 if (existsSync(testDbPath)) {
     unlinkSync(testDbPath);
 }
 if (existsSync(testDbJournalPath)) {
     unlinkSync(testDbJournalPath);
 }
+
+// temporarily rename prisma relevant files
+if (existsSync(migrationsFolder)) {
+    renameSync(migrationsFolder, tempMigrationsFolder);
+}
 if (existsSync(prismaEnvPath)) {
     renameSync(prismaEnvPath, tempPrismaEnvPath);
 }
-writeFileSync(prismaEnvPath, 'DATABASE_URL=file:./test.db')
+
+writeFileSync(prismaEnvPath, 'DATABASE_URL=file:./test.db');
