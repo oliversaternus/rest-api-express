699de9f1b5eec2087f66a675d55c7318a6d02805
oliversaternus <mail@oliversaternus.de>
2022-04-10 14:26:27 +0200

    rework email template

--- src/modules/email/types.ts
+++ src/modules/email/types.ts
@@ -3,8 +3,9 @@ export type TemplateKeys = 'ACCOUNT_VERIFICATION' | 'RESET_PASSWORD';
 export type TemplateProps = {
     ACCOUNT_VERIFICATION: {
         user: string;
-        company: string;
         verificationLink: string;
+        senderUrl: string;
+        senderCompanyAddress: string;
     },
     RESET_PASSWORD: {
         user: string;
